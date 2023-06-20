f1e097cd1cdcbd52abded85182c3bd619b8d14a8
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-06-20 17:41:33 +0300

    Added countries json and fixed select box to show the data

--- Server/Server/wwwroot/js/Registration.js
+++ Server/Server/wwwroot/js/Registration.js
@@ -1,15 +1,23 @@
 ﻿// JavaScript code to populate the countries in the select element
-$(document).ready(function () {
-    $.ajax({
-        url: 'https://api.myjson.com/bins/codku', // Replace with your API endpoint or JSON file URL
-        dataType: 'json',
-        success: function (data) {
-            var selectElement = $('#country');
-            $.each(data, function (key, value) {
-                var option = $('<option>').text(value.name);
-                selectElement.append(option);
-            });
-            selectElement.selectpicker('refresh');
-        }
-    });
-});
+// Function to populate the select element with country options
+function populateCountrySelect(countryData) {
+    var selectElement = document.getElementById('country');
+
+    for (var countryCode in countryData) {
+        var option = document.createElement('option');
+        option.value = countryCode;
+        option.text = countryData[countryCode];
+        selectElement.add(option);
+    }
+}
+
+// AJAX request to fetch the JSON data
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data/countries.json', true);
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        var countryData = JSON.parse(xhr.responseText);
+        populateCountrySelect(countryData);
+    }
+};
+xhr.send();
