2d65668dde0d7f91a3a56c95ae9f9f299d77bcf7
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-06-20 14:58:21 +0300

    Created css and javascript files for Regestration page

---
+++ Server/Server/wwwroot/js/Registration.js
@@ -0,0 +1,15 @@
+﻿// JavaScript code to populate the countries in the select element
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://api.myjson.com/bins/codku', // Replace with your API endpoint or JSON file URL
+        dataType: 'json',
+        success: function (data) {
+            var selectElement = $('#country');
+            $.each(data, function (key, value) {
+                var option = $('<option>').text(value.name);
+                selectElement.append(option);
+            });
+            selectElement.selectpicker('refresh');
+        }
+    });
+});
