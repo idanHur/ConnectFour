4542122b635e68b41562f5237a134b252f71f98b
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-06-20 18:47:53 +0300

    Create PlayerController.cs

---
+++ Server/Server/Controllers/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using GameManager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    public class PlayerController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Player model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Handles the form submission...
+
+                return RedirectToAction("Index", "Home"); // Redirect to a success page or a different page
+            }
+
+            // The model is invalid, return the view to display the validation messages.
+            return View(model);
+        }
+    }
+}
