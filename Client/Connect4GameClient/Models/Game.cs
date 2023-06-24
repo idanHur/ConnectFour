076d3ea9a244c26ba2ad96c0bf92cbaf48a96b32
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-06-24 18:38:50 +0300

    Added methods to Game

--- Client/Connect4GameClient/Models/Game.cs
+++ Client/Connect4GameClient/Models/Game.cs
@@ -16,10 +16,9 @@
     {
         public bool[,] board;
         public GameStatus gameStatus { get; private set; }
-        public PlayerType currentPlayer { get; private set; }
         public int gameId { get; private set; }
 
-        public ICollection<Move>? moves { get; set; }
+        public ICollection<Move> moves { get; set; }
 
         public Game(int rows, int columns, int gameId)
         {
@@ -28,10 +27,32 @@
             gameStatus = GameStatus.OnGoing;
             moves = new List<Move>();
         }
-        
-        public void MakeMove(int columns, bool isAi = false)
-        {
 
+        public void UpdateGameState(bool[,] board, GameStatus gameStatus, int lastMoveCol, PlayerType whichPlayer)
+        {
+            this.board = (bool[,])board.Clone();
+            this.gameStatus = gameStatus;
+            Move newMove = new Move(lastMoveCol, whichPlayer, moves.Count + 1);
+        }
+
+        public bool IsEligibleMove(int column)
+        {
+            // Check if the column is valid
+            if (column < 0 || column >= board.GetLength(1)) // use board size
+            {
+                throw new ArgumentOutOfRangeException("Column must be within the board size");
+            }
+
+            // Find the lowest empty row in the specified column
+            for (int row = board.GetLength(0) - 1; row >= 0; row--) // use board size
+            {
+                if (!board[row, column]) // if the cell is empty
+                {
+                    board[row, column] = true; // make the move
+                    return true;
+                }
+            }
+            return false;
         }
 
 
