b4a611fa5e09a277e6d716cc861bd6cc7cbddb8e
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-06-25 19:39:42 +0300

    Added code to create ellipse and method for when ellipse is clicked

--- Client/Client/ConnectFourWindow.xaml.cs
+++ Client/Client/ConnectFourWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Client.Helpers;
+using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,25 +24,50 @@ namespace Client
         public ConnectFourWindow()
         {
             InitializeComponent();
-            for (int row = 0; row < 6; row++)
-            {
-                for (int col = 0; col < 7; col++)
-                {
-                    var slot = new Ellipse
-                    {
-                        Width = 50,  // or any other size
-                        Height = 50,
-                        Fill = Brushes.White, // for an empty slot
-                        Stroke = Brushes.Black,
-                        StrokeThickness = 2
-                    };
-
-                    Grid.SetRow(slot, row);
-                    Grid.SetColumn(slot, col);
-
-                    this.LayoutRoot.Children.Add(slot);
-                }
-            }
+            
         }
+        private void AddGamePiece(int row, int col, Brush color, double opacity)
+        {
+            double ellipseHeight = 65;  // Height of the ellipse
+            double ellipseWidth = 66;  // Width of the ellipse
+            double topSpacing = 69;  // Spacing between the ellipses
+            double sideSpacing = 95;  // Spacing between the ellipses
+
+            var gamePiece = new Ellipse
+            {
+                Width = ellipseWidth,
+                Height = ellipseHeight,
+                Fill = color,  
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Opacity = opacity
+            };
+            // Set the row and column as attached properties on the ellipse
+            EllipseProperties.SetRow(gamePiece, row);
+            EllipseProperties.SetColumn(gamePiece, col);
+
+            gamePiece.MouseLeftButtonDown += Ellipse_MouseLeftButtonDown;
+
+            double topPosition = row * (ellipseWidth + sideSpacing);
+            double leftPosition = col * (ellipseHeight + topSpacing);
+            Canvas.SetTop(gamePiece, topPosition);
+            Canvas.SetLeft(gamePiece, leftPosition);
+
+            GameCanvas.Children.Add(gamePiece);
+        }
+
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var clickedEllipse = (Ellipse)sender;
+
+            // Get row and col
+            int row = EllipseProperties.GetRow(clickedEllipse);
+            int col = EllipseProperties.GetColumn(clickedEllipse);
+
+
+            
+        }
+
+
     }
 }
