b32877d85722360a7d053f5caaffe18d0fa4248e
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-06-25 18:46:52 +0300

    Added boardbackground for game window

---
+++ Client/Client/ConnectFourWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for ConnectFourWindow.xaml
+    /// </summary>
+    public partial class ConnectFourWindow : Window
+    {
+        public ConnectFourWindow()
+        {
+            InitializeComponent();
+            for (int row = 0; row < 6; row++)
+            {
+                for (int col = 0; col < 7; col++)
+                {
+                    var slot = new Ellipse
+                    {
+                        Width = 50,  // or any other size
+                        Height = 50,
+                        Fill = Brushes.White, // for an empty slot
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 2
+                    };
+
+                    Grid.SetRow(slot, row);
+                    Grid.SetColumn(slot, col);
+
+                    this.LayoutRoot.Children.Add(slot);
+                }
+            }
+        }
+    }
+}
