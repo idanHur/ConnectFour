74dae2eef1ba29bb3c594bd18bd82328fe22c82b
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-06-28 19:02:40 +0300

    Updated board logic
    Because we need to know which player has which slot and not only know if it is taken or not

--- Client/Connect4GameClient/Models/Game.cs
+++ Client/Connect4GameClient/Models/Game.cs
@@ -14,7 +14,7 @@
     }
     public class Game
     {
-        public bool[,] board;
+        public int[,] board;
         public GameStatus gameStatus { get; private set; }
         public int gameId { get; private set; }
 
@@ -23,19 +23,19 @@
         public Game(int rows, int columns, int gameId)
         {
             this.gameId = gameId;
-            board = new bool[rows, columns];
+            board = new int[rows, columns];
             gameStatus = GameStatus.OnGoing;
             moves = new List<Move>();
         }
 
-        public void UpdateGameState(bool[,] board, GameStatus gameStatus, int lastMoveCol, PlayerType whichPlayer)
+        public void UpdateGameState(int[,] board, GameStatus gameStatus, int lastMoveCol, PlayerType whichPlayer)
         {
-            this.board = (bool[,])board.Clone();
+            this.board = (int[,])board.Clone();
             this.gameStatus = gameStatus;
             Move newMove = new Move(lastMoveCol, whichPlayer, moves.Count + 1);
         }
 
-        public bool IsEligibleMove(int column)
+        public bool IsEligibleMove(int column, PlayerType player)
         {
             // Check if the column is valid
             if (column < 0 || column >= board.GetLength(1)) // use board size
@@ -46,9 +46,9 @@
             // Find the lowest empty row in the specified column
             for (int row = board.GetLength(0) - 1; row >= 0; row--) // use board size
             {
-                if (!board[row, column]) // if the cell is empty
+                if (board[row, column] == 0) // If the cell is empty
                 {
-                    board[row, column] = true; // make the move
+                    board[row, column] = player == PlayerType.Human ? 1 : 2; ; // Make the move if Human put 1 if Ai put 2
                     return true;
                 }
             }
