0248970ff0c92de70d6d0c7f9b2b989d18e36cb7
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-07-06 14:44:47 +0300

    Added custom Converters for move and game

---
+++ Client/Client/Utilities/Json/GameConveter.cs
@@ -0,0 +1,23 @@
+﻿using GameLogic.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Utilities.Json
+{
+    public class GameConveter : JsonConverter<Game>
+    {
+        public override Game ReadJson(JsonReader reader, Type objectType, Game existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, Game value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Client/Client/Utilities/Json/MoveConveter.cs
@@ -0,0 +1,23 @@
+﻿using GameLogic.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Utilities.Json
+{
+    public class MoveConveter : JsonConverter<Move>
+    {
+        public override Move ReadJson(JsonReader reader, Type objectType, Move existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, Move value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
