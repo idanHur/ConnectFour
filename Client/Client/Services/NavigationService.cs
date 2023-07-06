38c7462609d44b02c50856e23c9dfb49b01ddf65
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-07-06 22:33:28 +0300

    Added NavigationService to move between windows and still use dependency injection

---
+++ Client/Client/Services/NavigationService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Services
+{
+    public class NavigationService : INavigationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public NavigationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void NavigateToMain()
+        {
+            MainWindow mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+
+}
