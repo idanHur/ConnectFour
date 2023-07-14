57400fb46b6decbdb4146608cd88a6a3caf08e5c
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-07-14 21:31:46 +0300

    Added PlayersController and methods to return the data

---
+++ Server/Server/Controllers/PlayersController.cs
@@ -0,0 +1,69 @@
+﻿using GameManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Server.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private readonly Manager _manager;
+
+        public PlayersController(Manager manager)
+        {
+            _manager = manager;
+        }
+
+        public IActionResult Index()
+        {
+            var players = _manager.GetAllPlayers();
+            return View(players);
+        }
+
+        public IActionResult SortByName()
+        {
+            var players = _manager.GetAllPlayers()
+                .OrderBy(p => p.playerName.ToLower())
+                .ToList();
+
+            return View(players);  // Use "Index" view to display sorted players
+        }
+
+        public IActionResult SortByNameDescending()
+        {
+            var players = _manager.GetAllPlayers()
+                .OrderByDescending(p => p.playerName.ToLower())
+                .ToList();
+
+            return View(players);  // Use "Index" view to display sorted players
+        }
+
+        public IActionResult AllGames()
+        {
+            var games = _manager.GetAllPlayers()
+                .SelectMany(p => p.games)
+                .ToList();
+
+            return View(games);
+        }
+
+        public IActionResult PlayerGameCounts()
+        {
+            var playerGameCounts = _manager.GetAllPlayers()
+                .Select(p => new { PlayerName = p.playerName, GameCount = p.games.Count })
+                .ToList();
+
+            return View(playerGameCounts);
+        }
+
+        public IActionResult PlayersSortedByGameCount()
+        {
+            var players = _manager.GetAllPlayers()
+                .OrderByDescending(p => p.games.Count)
+                .ToList();
+
+            return View(players);
+        }
+    }
+}
