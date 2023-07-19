9dc33c25d38783f53fede0e57695d7de74e2ae54
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-07-19 15:33:39 +0300

    Added select menu

--- Server/Server/Pages/EditPlayer.cshtml.cs
+++ Server/Server/Pages/EditPlayer.cshtml.cs
@@ -1,12 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
+using GameManager.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Server.Pages
 {
     public class EditPlayerModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Player EditPlayer { get; set; }
+
+        [BindProperty]
+        public int SelectedPlayerId { get; set; }
+
+        public List<Player> Players { get; set; }
+
+        private readonly Manager _gameManager;
+
+        public EditPlayerModel(Manager gameManager)
         {
+            _gameManager = gameManager;
+            Players = _gameManager.GetAllPlayers();
         }
+
+        public void OnGet(int? id)
+        {
+            if (id.HasValue)
+            {
+                // Get player details based on the id
+                EditPlayer = _gameManager.GetPlayer(id.Value);
+                SelectedPlayerId = EditPlayer.playerId;
+            }
+        }
+
+      
     }
 }
