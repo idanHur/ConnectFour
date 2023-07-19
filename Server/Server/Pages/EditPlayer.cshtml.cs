560fb2f29eba09e2bd45eee6991967a1688698b0
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-07-19 15:27:31 +0300

    Added edit player page

--- Server/Server/Pages/EditPlayer.cshtml.cs
+++ Server/Server/Pages/EditPlayer.cshtml.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Server.Pages
 {
-    public class PrivacyModel : PageModel
+    public class EditPlayerModel : PageModel
     {
         public void OnGet()
         {
